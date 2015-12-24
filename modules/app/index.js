8306ee49aff3e594c38326873ff122b067a96caa
yanhaijing <yanhaijing@yeah.net>
2015-12-24 22:21:09 +0800

    添加对各个组件的测试js

--- modules/app/index.js
+++ modules/app/index.js
@@ -1,7 +1,22 @@
 var $ = require('jquery');
+
+// 测试UIbase
 var UIBase = require('ui/base/base');
+console.log(UIBase);
+
+// 测试Promise
+var Promise = require('es6-promise').Promise;
+console.log(Promise);
+
+// 测试template
 require('util/template/template');
 var tpl = __inline('test.tmpl');
-
 $('body').append(tpl({}));
-console.log(UIBase);
+
+// 测试 zepto
+var zepto = require('zepto');
+console.log(zepto);
+
+// 测试 underscore
+var _ = require('underscore');
+console.log(_)
